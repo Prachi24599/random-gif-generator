f46f413a319d30a3219b03254940e52779a60bc7
Prachi24599 <prachi24599@gmail.com>
2023-07-11 11:41:58 +0530

    Heading and components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import Random from "./components/Random";
+import Tag from "./components/Tag";
+
 export default function App() {
-  return <div>Random GIF</div>;
+  return (
+    <div className="w-full h-screen flex flex-col background">
+      <h1 className="bg-white rounded-lg text-center mt-[40px] ml-[25px] mr-[25px] font-bold text-3xl py-2">
+        RANDOM GIFS
+      </h1>
+      <div className="flex flex-col">
+        <Random />
+        <Tag />
+      </div>
+    </div>
+  );
 }
