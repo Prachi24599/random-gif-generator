3115611f88fa57883e8afe58b4d27e31d721cf6e
Prachi24599 <prachi24599@gmail.com>
2023-07-13 13:04:48 +0530

    calling API to get gif url

--- src/components/Random.js
+++ src/components/Random.js
@@ -1,18 +1,33 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const Random = () => {
   const [gif, setGif] = useState("");
 
-  function clickHandler() {}
+  async function fetchData() {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    const { data } = await axios.get(url);
+    const imageSource = data.data.images.downsized_large.url;
+    setGif(imageSource);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function clickHandler() {
+    fetchData();
+  }
 
   return (
-    <div className="w-1/2 h-[450px] bg-green-400 rounded-lg border border-black flex flex-col items-center gap-y-5">
+    <div className="w-1/2 bg-green-400 rounded-lg border border-black flex flex-col items-center gap-y-5">
       <h1 className="text-2xl underline uppercase font-bold mt-2">
         A Random Gif
       </h1>
       <img src={gif} alt="random_gif" width="450" />
       <button
-        className="w-10/12 bg-white text-lg py-2 rounded-lg uppercase font-semibold"
+        className="w-10/12 bg-white text-lg py-2 rounded-lg uppercase font-semibold mb-[20px]"
         onClick={clickHandler}
       >
         Generate
