5bb57e818a705f41058f3b7a3dc655ac09bb6784
Prachi24599 <prachi24599@gmail.com>
2023-07-13 16:01:05 +0530

    custom hook

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,29 +1,13 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Spinner from "./Spinner";
 import useGif from "../hooks/useGif";
 
-const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 const Tag = () => {
   const [tag, setTag] = useState("car");
-  // const [gif, setGif] = useState("");
-  // const [loading, setLoading] = useState(false);
-  // async function fetchData() {
-  //   setLoading(true);
-  //   const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-  //   const { data } = await axios.get(url);
-  //   const imageSource = data.data.images.downsized_large.url;
-  //   setGif(imageSource);
-  //   setLoading(false);
-  // }
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  const { gif, loading, fetchData } = useGif();
+  const { gif, loading, fetchData } = useGif(tag);
 
   return (
-    <div className="w-1/2 bg-blue-400 rounded-lg border border-black flex flex-col items-center gap-y-5">
+    <div className="w-1/2 bg-blue-400 rounded-lg border border-black flex flex-col items-center gap-y-5 mb-[40px]">
       <h1 className="text-2xl underline uppercase font-bold mt-2">
         Random {tag} Gif
       </h1>
@@ -37,7 +21,7 @@ const Tag = () => {
       />
       <button
         className="w-10/12 bg-white text-lg py-2 rounded-lg uppercase font-semibold mb-[20px]"
-        onClick={() => fetchData()}
+        onClick={() => fetchData(tag)}
       >
         Generate
       </button>
--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,25 +1,23 @@
-import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-const randomMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-const tagMemeUrl = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
-const useGif = () => {
+const useGif = (tag) => {
   const [gif, setGif] = useState("");
   const [loading, setLoading] = useState(false);
 
-  async function fetchData() {
+  async function fetchData(tag) {
     setLoading(true);
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(tag ? `${url}&tag=${tag}` : url);
     const imageSource = data.data.images.downsized_large.url;
     setGif(imageSource);
     setLoading(false);
   }
 
   useEffect(() => {
-    fetchData();
+    fetchData("car");
   }, []);
 
   return { gif, loading, fetchData };
